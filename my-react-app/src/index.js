98446b92758ca7f2e1537e2f84be664ebe125169
Xiaotian <58612496+Xwang11@users.noreply.github.com>
2022-10-20 10:38:32 -0400

    Testing

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-class Car {
-  constructor(name) {
-    this.brand = name;
-  }
-  present() {
-    return  "I have a " + this.brand;
-  }
-}
-class Model extends Car {
-  constructor(name, mod) {
-    super(name);
-    this.model = mod;
-  }
-  show() {
-    return this.present() + ", it is a " + this.model
-  }
-}
-
-const myArray = ["apples", "bannanas", "oranges", "omg fruits"];
-const myList = myArray.map((item) => <p>{item}</p>);
-
-const mycar = new Model("Ford", "Mustang")
-
-const myElement = <h1>{myList}</h1>;
+const title = (
+  <center><h1>Happy Birthday!</h1>
+  <h2>To the best mom in the world</h2></center>
+  
+);
+const subtitle = (
+  <center><h2>To the best mom in the world</h2></center>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myElement);
+root.render(title);
+
+
+
+
+
+// class Car {
+//   constructor(name) {
+//     this.brand = name;
+//   }
+//   present() {
+//     return  "I have a " + this.brand;
+//   }
+// }
+// class Model extends Car {
+//   constructor(name, mod) {
+//     super(name);
+//     this.model = mod;
+//   }
+//   show() {
+//     return this.present() + ", it is a " + this.model
+//   }
+// }
+
+// const myArray = ["apples", "bannanas", "oranges", "omg fruits"];
+// const myList = myArray.map((item) => <p>{item}</p>);
+
+// const mycar = new Model("Ford", "Mustang")
+
+// const myElement = <h1>{myList}</h1>;
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myElement);
